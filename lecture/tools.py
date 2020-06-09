d34f32d883063f4d0339451f5002319825cb952e
Roelof Rietbroek <roelof@peinzpad>
2020-06-09 12:27:42 +0200

    Added the FlexSlide class and moved the python covnenvience functions to a local python module

---
+++ lecture/tools.py
@@ -0,0 +1,127 @@
+# convenience tools for setting up lecture slides
+
+
+from IPython.display import Video,display_html,display_markdown,HTML,Markdown,display
+
+import binascii
+
+import os
+
+import re
+
+from lxml import etree as ET
+
+from IPython.display import Markdown
+
+
+
+def hideCode():   
+    """Hides the code section of a jupyter notebook cell and only keep the output
+    see also https://www.markroepke.me/posts/2019/06/05/tips-for-slideshows-in-jupyter.html"""
+
+    uid = binascii.hexlify(os.urandom(8)).decode()
+    html = """<div id="%s"></div>
+    <script type="text/javascript">
+        $(function(){
+            var p = $("#%s");
+            if (p.length==0) return;
+            while (!p.hasClass("cell")) {
+                p=p.parent();
+                if (p.prop("tagName") =="body") return;
+            }
+            var cell = p;
+            cell.find(".input").addClass("hide-in-slideshow")
+        });
+    </script>""" % (uid, uid)
+    display_html(html, raw=True)
+
+
+
+class FlexSlide():
+    """Class which setups the cell output as a flex container, which allows more flexible structuring (see also the rise.css file)"""
+    def __init__(self,title):
+        hideCode()
+        self.mdhead=title+"\n<div class=\"flxsld text_cell\" >\n"
+        self.mdfoot="\n</div>"
+        self.payload=""  
+
+         
+
+    def addimg(self,path,caption="",flxwidth=None,width=None,frag=False):
+        """Adds an image"""
+        addcap=""
+        cls=""
+        if flxwidth:
+            cls="class=\"%s\""%flxwidth
+        if caption:
+            addcap="<small>%s</small>"%caption
+
+        if width:
+            md="<div %s><img src=\"%s\" alt=\"%s\" style=\"width:%s;\" />%s</div>"%(cls,path,caption,width,addcap)
+        else:
+            md="<div %s><img src=\"%s\" alt=\"%s\" />%s</div>"%(cls,path,caption,addcap)
+
+        self.addmd(md,frag=frag)
+            
+
+    def addmd(self,mdcontent,frag=False,flxwidth=None):
+        cls=""
+
+        if flxwidth:
+            cls+=flxwidth
+        if frag:
+            cls+=" fragment fade-in"
+
+        self.payload+="\n<div class=\"%s\">\n\n"%(cls)+mdcontent+"</div>"
+
+
+    def addVideo(self,path,width=None):
+        self.addmd(Video(path)._repr_html_(),width=width)
+        
+
+    def addItems(self,items,frag=False,flxwidth=None):       
+        html="<ul>"
+
+        for item in items:
+            if frag:
+                html+="<li class=\"fragment fade-in\">%s</li>"%item
+            else:
+                html+="<li>%s</li>"%item
+
+        html+="</ul>"
+        self.addmd(html,flxwidth=flxwidth)
+
+    def addSVG(self,svgname,width=None,height=None):
+        """Embed SVG as code in a code cell and fix relative links"""
+
+        svgroot = ET.parse(svgname).getroot()
+        if width:
+            try:
+                svgroot.attrib["width"]=width
+                del svgroot.attrib["height"]
+            except KeyError:
+                pass
+        elif height:
+            try:
+                svgroot.attrib["height"]=height
+                del svgroot.attrib["width"]
+            except KeyError:
+                pass
+
+
+        #change absolute embedded image links to relative links
+
+        hrefky="{"+svgroot.nsmap["xlink"]+"}href"
+        
+        for el in svgroot.findall(".//{*}image[@"+hrefky+"]"):
+            el.attrib[hrefky]=re.sub("\S+/images/","images/",el.attrib[hrefky])
+
+        self.payload+="\n<div \">\n\n"+ET.tostring(svgroot).decode('utf-8')+"</div>"
+    def _repr_markdown_(self):
+        # print("\n".join([self.mdhead,self.payload,self.mdfoot]))
+
+        return "\n".join([self.mdhead,self.payload,self.mdfoot])
+
+    
+    def display(self):
+        display(self)
